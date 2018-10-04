2fba2beeebe94a3f6c0ef6ab3a69b1b0920b0735
Yevstaf <yevstafglobal@gmail.com>
2018-10-04 13:26:45 +0300

    interface Update

--- app/src/main/java/com/example/yevstaf/checkthesun/adapter_factories/InfoCardsListAbstractFactory.java
+++ app/src/main/java/com/example/yevstaf/checkthesun/adapter_factories/InfoCardsListAbstractFactory.java
@@ -2,20 +2,16 @@ package com.example.yevstaf.checkthesun.adapter_factories;
 
 import android.content.Context;
 import android.widget.SimpleAdapter;
-
 import com.example.yevstaf.checkthesun.R;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Vladyslav on 24.09.2018.
  */
 
-public class ListViewAdaptersAbstractFactory  {
+public class InfoCardsAbstractFactory {
     Context context;
 
-    public ListViewAdaptersAbstractFactory(Context context) {
+    public InfoCardsAbstractFactory(Context context) {
         this.context = context;
     }
 
---
+++ app/src/main/java/com/example/yevstaf/checkthesun/interface_click_listeners/OnInfoCardsListItemClickListener.java
@@ -0,0 +1,8 @@
+package com.example.yevstaf.checkthesun.interface_click_listeners;
+
+/**
+ * Created by Vladyslav on 04.10.2018.
+ */
+
+public class OnInfoCardsListItemClickListener {
+}
